13382ace53a991f54d53c3a59b0fbf57ca6b5f51
Taly Adler <redacted>
2025-05-16 13:48:44 +0300

    start

---
+++ src/export/exproter.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template<typename T> class EXPORTER {
+private:
+public:
+    /*
+    CONSTRUTORS
+    */
+    EXPORTER(){}
+    /*
+    DESTRUCTORS
+    */
+    ~EXPORTER(){}
+    
+    /*
+    METHODS
+    */
+    void make();
+};
